8634f0be1ed4385796422b78d5a52aaf05df67cf
Lan <lanngoctran92@gmail.com>
2025-11-27 23:09:00 +0700

    [ID - 08] Implement test - Create new product part 1

--- scripts/commonPage.ts
+++ scripts/commonPage.ts
@@ -12,8 +12,22 @@ export class CommonPage {
     await this.page.locator(xpath).fill(value);
   }
 
+  async inputTextAreaByLabel(label: string, value: string) {
+    let xpath = `(//label[normalize-space(text())="${label}"]//following::textarea)[1]`;
+    await this.page.locator(xpath).fill(value);
+  }
+
   async clickMenuByLabel(label: string) {
     let xpath = `//div[contains(concat(' ',normalize-space(@class),' '),' admin-nav ')]//a[normalize-space(text())="${label}"]`;
     await this.page.locator(xpath).click();
+  }
+
+  async selectDropdownByLabel(label: string, option: string) {
+    let xpath1 = `(//label[normalize-space(text())="${label}"]//following::select)[1]`;
+    let xpath2 = `(//h3[normalize-space(text())="${label}"]//following::select)[1]`;
+    let xpath3 = `(//td[normalize-space(text())="${label}"]//following::select)[1]`;
+    await this.page
+      .locator(`${xpath1} | ${xpath2} | ${xpath3}`)
+      .selectOption(option);
   }
 }
--- tests/new_product.spec.ts
+++ tests/new_product.spec.ts
@@ -16,4 +16,29 @@ test("Create new product", async ({ page }) => {
 
   let xpathHeading = `//h1[normalize-space(text())="Create a new product"]`;
   await expect(page.locator(xpathHeading)).toBeVisible();
+  await newProductPage.inputTextByLabel(
+    "Name",
+    "Giày Thể Thao Biti's Hunter X Nam Màu Đen"
+  );
+  await newProductPage.inputTextByLabel("SKU", "HSM011700DEN");
+  await newProductPage.inputTextByLabel("Price", "800000");
+  await newProductPage.inputTextByLabel("Weight", "0.2");
+
+  await newProductPage.selectDropdownByLabel("Tax class", "Taxable Goods");
+
+  await newProductPage.inputTextByLabel("Quantity", "100");
+  await newProductPage.inputTextByLabel(
+    "Url key",
+    "giay-the-thao-biti-s-hunter-x-nam-mau-den-hsm011700den"
+  );
+  await newProductPage.inputTextByLabel("Meta title", "Giay bitis");
+  await newProductPage.inputTextByLabel("Meta keywords", "bitis den");
+  await newProductPage.inputTextAreaByLabel(
+    "Meta description",
+    "Giay bitis den"
+  );
+
+  await newProductPage.selectDropdownByLabel("Attribute group", "Default");
+  await newProductPage.selectDropdownByLabel("Color", "White");
+  await newProductPage.selectDropdownByLabel("Size", "XL");
 });
