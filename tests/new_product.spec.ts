a387fe26dcd5e0a3c2bdb123f62f8e1009328c79
Lan <lanngoctran92@gmail.com>
2025-12-18 22:03:27 +0700

    [ID - 13] Simulate server error

--- tests/new_product.spec.ts
+++ tests/new_product.spec.ts
@@ -145,3 +145,61 @@ test("Create new product 2", async ({ page }) => {
   expect(await productPage.getInputValueByLabel("Name")).toEqual(productName);
   expect(await productPage.getInputValueByLabel("SKU")).toEqual(sku);
 });
+
+test("Verify error message when server return 500", async ({ page }) => {
+  let random = new Date().getTime();
+  // await loginPage.adminLogin();
+  await newProductPage.clickMenuByLabel("New Product");
+
+  await expect(page.locator(newProductPage.xpathHeading)).toBeVisible();
+  let productName = `Giày Thể Thao Biti's Hunter X Nam Màu Đen ${random}`;
+  await newProductPage.inputTextByLabel("Name", productName);
+  let sku = `SKU-${random}`;
+  await newProductPage.inputTextByLabel("SKU", sku);
+  await newProductPage.inputTextByLabel("Price", "800000");
+  await newProductPage.inputTextByLabel("Weight", "0.2");
+
+  await newProductPage.selectDropdownByLabel("Tax class", "Taxable Goods");
+
+  await newProductPage.uploadProductImage("data/images/bitis.jpg");
+
+  await newProductPage.inputTextByLabel("Quantity", "100");
+  await newProductPage.inputTextByLabel(
+    "Url key",
+    `giay-the-thao-biti-s-hunter-x-nam-mau-den-hsm${random}`
+  );
+  await newProductPage.inputTextByLabel("Meta title", "Giay bitis");
+  await newProductPage.inputTextByLabel("Meta keywords", "bitis den");
+  await newProductPage.inputTextAreaByLabel(
+    "Meta description",
+    "Giay bitis den"
+  );
+
+  await newProductPage.selectDropdownByLabel("Attribute group", "Default");
+  await newProductPage.selectDropdownByLabel("Color", "White");
+  await newProductPage.selectDropdownByLabel("Size", "XL");
+  await newProductPage.selectRadioByLabel("Status", "Disabled");
+  await newProductPage.selectRadioByLabel("Visibility", "Not visible");
+  await newProductPage.selectRadioByLabel("Manage stock?", "No");
+  await newProductPage.selectRadioByLabel("Stock availability", "No");
+
+  //Modify response
+  page.route("**/api/products", async (route) => {
+    // Fetch original response.
+    // const response = await route.fetch();
+
+    await route.fulfill({
+      json: {
+        error: {
+          status: 500,
+          message: "Internal Server Error",
+        },
+      },
+      //Modify status code
+      status: 500,
+    });
+  });
+  await newProductPage.clickButtonByLabel("Save");
+  await page.waitForTimeout(10000);
+  await newProductPage.verifyPopupMessage("Internal Server Error");
+});
