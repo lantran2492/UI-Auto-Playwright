dea9c84ee9ab100e5cbbc349c2724035e5d89481
Lan <lanngoctran92@gmail.com>
2025-12-20 22:16:25 +0700

    [ID - 16] Handle multiple environment

--- scripts/utils/constant.ts
+++ scripts/utils/constant.ts
@@ -1,5 +1,14 @@
-export const UI_HOST = "http://localhost";
-export const UI_PORT = "3000";
+import dotenv from "dotenv";
+
+let currentEnv = process.env.TEST_ENV;
+if (currentEnv == "sit1") {
+  dotenv.config({ path: "env/.env.sit1" });
+} else {
+  dotenv.config({ path: "env/.env.local" });
+}
+
+export const UI_HOST = process.env.UI_HOST;
+export const UI_PORT = process.env.UI_PORT;
 export const UI_URL = `${UI_HOST}:${UI_PORT}`;
 export const UI_ADMIN_PATH = `${UI_URL}/admin/login`;
 export const UI_PRODUCT_PATH = `${UI_URL}/api/products`;
